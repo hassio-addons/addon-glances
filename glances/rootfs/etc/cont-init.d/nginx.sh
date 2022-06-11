5d59f3fcb67a594480ebca0da2b9cc4b4a7d3043
Franck Nijhof <git@frenck.dev>
2022-06-11 12:29:52 +0200

    ⬆️ Upgrades add-on base image to 12.0.0

--- glances/rootfs/etc/cont-init.d/nginx.sh
+++ glances/rootfs/etc/cont-init.d/nginx.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/with-contenv bashio
+#!/usr/bin/env bashio
 # ==============================================================================
 # Home Assistant Community Add-on: SSH & Web Terminal
 # Configures NGINX for use with ttyd
