127321f0130d14e081bac8ec89c56a1bdaa583a1
Brett Miller <github@shadowed.net>
2022-12-28 12:45:40 -0600

    add support for influxdb2

--- glances/rootfs/etc/cont-init.d/glances.sh
+++ glances/rootfs/etc/cont-init.d/glances.sh
@@ -35,3 +35,21 @@ if bashio::config.true 'influxdb.enabled'; then
         echo "protocol=${protocol}"
     } >> /etc/glances.conf
 fi
+
+if bashio::config.true 'influxdb2.enabled'; then
+    protocol='http'
+    if bashio::config.true 'influxdb.ssl'; then
+    protocol='https'
+    fi
+    # Modify the configuration
+    {
+        echo "[influxdb2]"
+        echo "host=$(bashio::config 'influxdb2.host')"
+        echo "port=$(bashio::config 'influxdb2.port')"
+        echo "user=$(bashio::config 'influxdb2.org')"
+        echo "password=$(bashio::config 'influxdb2.token')"
+        echo "db=$(bashio::config 'influxdb2.bucket')"
+        echo "prefix=$(bashio::config 'influxdb2.prefix')"
+        echo "protocol=${protocol}"
+    } >> /etc/glances.conf
+fi
